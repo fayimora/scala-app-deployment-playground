403c8c4b935cdab3d8c2ad24c1bdde9c007e03e2
Fayi Femi-Balogun <fayi@fayimora.com>
2022-06-18 21:12:18 +0100

    Add hi endpoint
    - testing new docker version

--- src/main/scala/com/fayimora/sadp/Main.scala
+++ src/main/scala/com/fayimora/sadp/Main.scala
@@ -8,6 +8,8 @@ object Main extends ZIOAppDefault:
   val app = Http.collect[Request] {
     case Method.GET -> !! / "hello" =>
       Response.text("Hello World!")
+    case Method.GET -> !! / "hi" =>
+      Response.text("Hi World!")
   }
 
   override def run =
